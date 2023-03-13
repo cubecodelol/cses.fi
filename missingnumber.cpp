237d7c05ed7cc282cbe58ac0d7b9552f82d4e2d2
Hakan <123600510+cubemanlol@users.noreply.github.com>
2023-03-13 06:49:00 -0700

    Add files via upload

---
+++ missingnumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#define ll long long
+using namespace std;
+int main() {
+    ll n,s=0,swm=0;
+    cin>>n;
+    for(int i=1; i<=n; i++) {
+        s+=i;
+    }
+    for(int i=1; i<n; i++) {
+        ll a;
+        cin>>a;
+        swm+=a;
+    }
+    cout<<s-swm;
+}
